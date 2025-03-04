73d36c6fdac2bcfb3695db5709f2328a519b0e70
Boru Isako <isco.jaldy@gmail.com>
2025-03-04 14:49:26 +0300

    Complete labs

---
+++ index.js
@@ -0,0 +1,9 @@
+function divide(){
+   let num1=2000
+   let num2=100
+   return num1/num2
+}
+const square = num => num*num
+console.log(square(2))
+const add = (num1,num2) => num1+num2
+console.log(add(2,3))
